f04c63fb5076095c386536ec326c53131ba33dc6
behmah <b2000yasini@gmail.com>
2024-11-16 15:16:22 +0430

    income model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,3 +8,13 @@ class Expense(models.Model):
     amount = models.BigIntegerField()
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} - {}".format(self.text, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{} - {}".format(self.text, self.amount)
